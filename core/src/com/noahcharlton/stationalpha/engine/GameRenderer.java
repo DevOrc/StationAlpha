a9ad3cf0b8dc8aa8459344fb7b8c3ac23b2a8a4d
DevOrc <ncharlton002@gmail.com>
2019-03-02 20:01:52 -0500

    Add world haze

--- core/src/com/noahcharlton/stationalpha/engine/GameRenderer.java
+++ core/src/com/noahcharlton/stationalpha/engine/GameRenderer.java
@@ -1,10 +1,12 @@
 package com.noahcharlton.stationalpha.engine;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.noahcharlton.stationalpha.engine.input.InputHandler;
 import com.noahcharlton.stationalpha.worker.WorkerRenderer;
 import com.noahcharlton.stationalpha.world.World;
 
@@ -32,8 +34,21 @@ public class GameRenderer {
         updateCamera();
 
         spriteBatch.begin();
+        renderWorldHaze();
         World.getInstance().ifPresent(world -> world.render(spriteBatch));
         spriteBatch.end();
+    }
+
+    private void renderWorldHaze() {
+        int thickness = 8;
+        int worldWidth = World.WORLD_PIXEL_SIZE + thickness;
+
+        if(InputHandler.getInstance().getBuildManager().getAction().isPresent()){
+            ShapeUtil.drawRect(-thickness, -thickness, thickness, worldWidth, Color.BLUE, spriteBatch);
+            ShapeUtil.drawRect(-thickness , -thickness, worldWidth, thickness, Color.BLUE, spriteBatch);
+            ShapeUtil.drawRect(worldWidth - thickness, -thickness, thickness, worldWidth, Color.BLUE, spriteBatch);
+            ShapeUtil.drawRect(-thickness, worldWidth - thickness, worldWidth, thickness, Color.BLUE, spriteBatch);
+        }
     }
 
     private void clearScreen() {
