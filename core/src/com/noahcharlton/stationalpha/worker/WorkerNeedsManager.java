1dff8ad6484cd0fd7b60156f40de1e7000b1bb32
DevOrc <ncharlton002@gmail.com>
2019-08-31 21:09:47 -0400

    Add zzz to worker texture when not sleeping in a bed

--- core/src/com/noahcharlton/stationalpha/worker/WorkerNeedsManager.java
+++ core/src/com/noahcharlton/stationalpha/worker/WorkerNeedsManager.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public class WorkerNeedsManager {
 
     public static final int FOOD_TIME = 1200;
-    public static final int SLEEP_RESET = 6000;
+    public static final int SLEEP_RESET = 150;
 
     private final Worker worker;
 
