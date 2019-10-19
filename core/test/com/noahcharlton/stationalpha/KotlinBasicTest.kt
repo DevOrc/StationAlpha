c2297da2a10860efe9e46981024c87f96ee9417b
DevOrc <ncharlton002@gmail.com>
2019-10-19 08:42:04 -0400

    Add Kotlin and screenshots

---
+++ core/test/com/noahcharlton/stationalpha/KotlinBasicTest.kt
@@ -0,0 +1,13 @@
+package com.noahcharlton.stationalpha
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class KotlinBasicTest{
+
+    @Test
+    fun kotlinBasicTest(){
+        Assertions.assertTrue(true)
+    }
+
+}
