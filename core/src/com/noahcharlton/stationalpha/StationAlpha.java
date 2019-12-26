2b5b15283fca274d60218baea35a001c56a99361
DevOrc <ncharlton002@gmail.com>
2019-12-26 18:26:30 -0500

    Release version 0.3.0

--- core/src/com/noahcharlton/stationalpha/StationAlpha.java
+++ core/src/com/noahcharlton/stationalpha/StationAlpha.java
@@ -29,7 +29,7 @@ public class StationAlpha extends ApplicationAdapter {
 
     public enum GameState {LOADING, MAIN_MENU, IN_GAME}
 
-    public static final String VERSION = "0.3.0-dev";
+    public static final String VERSION = "0.3.0";
     public static final int DEFAULT_WIDTH = 1280;
     public static final int DEFAULT_HEIGHT = 720;
 
